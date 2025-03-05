6d007bcfa3d7e3b565fe4e03671c183d5d5f4edc
ibrahimeltbakh <eltabakh987@gmail.com>
2025-03-05 19:09:57 +0200

    update home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,9 +13,10 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xff85AAA3),
-        leading: Icon(Icons.menu),
-        title: Text("Hello Ibrahim"),
+        backgroundColor: Color.fromARGB(255, 32, 228, 192),
+
+        leading: Icon(Icons.menu, color: Colors.white),
+        title: Text("Hello Ibrahim", style: TextStyle(color: Colors.white)),
         centerTitle: true,
         actions: [
           IconButton(
@@ -24,10 +25,62 @@ class _HomeScreenState extends State<HomeScreen> {
                 counter++;
               });
             },
-            icon: Icon(Icons.star),
+            icon: Icon(Icons.star, color: Colors.white),
           ),
-          IconButton(onPressed: () {}, icon: Text('$counter')),
+          IconButton(
+            onPressed: () {},
+            icon: Text('$counter', style: TextStyle(color: Colors.white)),
+          ),
         ],
+      ),
+
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.teal,
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.red,
+                child: Center(child: Text("one")),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 50,
+                        color: Colors.yellow,
+                        child: Center(child: Text("two")),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                        color: Colors.green,
+                        child: Center(child: Text("three")),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.blue,
+                child: Center(child: Text("four")),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
